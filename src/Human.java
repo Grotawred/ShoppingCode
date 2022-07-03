cb0502afe12295cb426dd178d31cf2abbd5f58f6
Grotawred <grishavlasko200@gmail.com>
2022-07-03 18:06:54 +0300

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Human {
+    protected ArrayList<String> purchaseList = new ArrayList<>();
+
+    public ArrayList<String> getPurchaseList() {
+        return purchaseList;
+    }
+
+    public void setPurchaseList(ArrayList<String> purchaseList) {
+        this.purchaseList = purchaseList;
+    }
+}
